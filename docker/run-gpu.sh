935482909dc4433aab3a49b5d879f086699b2aa9
Cenk Bircanoglu <cenk.bircanoglu@adevinta.com>
2019-12-05 12:52:39 +0100

    dockerfile updated

--- docker/run-gpu.sh
+++ docker/run-gpu.sh
@@ -4,4 +4,4 @@ mkdir output
 
 docker build -t yolo34py-gpu -f Dockerfile-gpu .
 
-nvidia-docker run --rm -it --name yolo34py-gpu -v `pwd`/input:/YOLO3-4-Py/input -v `pwd`/output:/YOLO3-4-Py/output yolo34py-gpu
+nvidia-docker run --rm -it --name yolo34py-gpu -p 80:5000

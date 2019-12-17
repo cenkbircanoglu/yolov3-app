1ae5db48471173815b1d41c04339531501f64b6b
Cenk Bircanoglu <cenk.bircanoglu@adevinta.com>
2019-12-17 17:34:54 +0100

    update name of the docker images

--- run-gpu.sh
+++ run-gpu.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-nvidia-docker run -d --rm -it --name cenkbircanoglu/yolo34py-gpu:latest -p 80:5000 yolo34py-gpu
+nvidia-docker run -d --rm -it --name cenkbircanoglu/yolo34py-gpu -p 80:5000 yolo34py-gpu

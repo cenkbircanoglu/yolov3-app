7624b7e67247c5437ccdab71577b43aba0be62e0
Cenk Bircanoglu <cenk.bircanoglu@adevinta.com>
2019-12-17 17:37:33 +0100

    update name of the docker images

--- run-cpu.sh
+++ run-cpu.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Run the docker image
-docker run --rm -it --name cenkbircanoglu/yolo34-py -p 80:5000 yolo34-py
+docker run --rm -it --name yolo34-py -p 80:5000 cenkbircanoglu/yolo34-py
